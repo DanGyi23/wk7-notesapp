929fb5264bfae4461f4304bfcf2754be0e3e50a3
Dan Gyi <daniel.gyi@gmail.com>
2019-10-02 15:47:53 +0100

    refactors controller for notelist injection

--- controller/note-controller.js
+++ controller/note-controller.js
@@ -1,21 +1,17 @@
 (function (exports) {
 
-  function noteController() {
-    this._notelist = new NoteList;
+  function noteController(notelist) {
+    this._notelist = notelist;
     this._notelistview = new NoteListView(this._notelist);
-    
-    this._notelist.addNote("Zip Zop");
-    this._notelist.addNote("1234567890123456789012345");
 
     new_html = this._notelistview.returnListView();
     element = document.getElementById('app')
     element.innerHTML = new_html
 
-    var listofnotes = this._notelist
     window.addEventListener("hashchange", function (event) {
       event.preventDefault();
       var current_id = location.hash.split("/")[1]
-      var noteobject = listofnotes.list.find(function (element) {
+      var noteobject = notelist.list.find(function (element) {
         return element.id == current_id
       })
       document.getElementById('note-text').innerHTML = noteobject.text
--- test/note-controller-tests.js
+++ test/note-controller-tests.js
@@ -1,15 +1,16 @@
 function testNoteControllerInstantiation() {
-  var notecontroller = new noteController();
+  var notelist = new NoteList();
+  var notecontroller = new noteController(notelist);
   assert.isTrue(notecontroller !== undefined);
   console.log("testNoteControllerInstantiation passed")
 };
 
 function testInnerHTMLProperty() {
-  var notecontroller = new noteController();
+  var notelist = new NoteList();
+  var notecontroller = new noteController(notelist);
   assert.isTrue(notecontroller._notelistview.returnListView() == "<ul></ul>");
   console.log("testInnerHTMLProperty passed")
 };
 
 testNoteControllerInstantiation();
 testInnerHTMLProperty();
-testNoteControllerHashChange();
