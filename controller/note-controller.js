17042aaa5b1460b0bd7fa061aaf34ed8c087af26
Dan Gyi <daniel.gyi@gmail.com>
2019-10-02 11:50:41 +0100

    update controller with new module

--- controller/note-controller.js
+++ controller/note-controller.js
@@ -1,6 +1,16 @@
-function changeAppText() {
-  element = document.getElementById("app");
-  element.innerHTML = 'Howdy'
-};
+(function(exports) {
 
-changeAppText();
+  function changeAppHTML(text) {
+    var notelist = new NoteList;
+    notelist.addNote(text);
+    var notelistview = new NoteListView(notelist);
+
+    new_html = notelistview.returnListView();
+    element = document.getElementById('app')
+    element.innerHTML = new_html
+  }
+ 
+  exports.changeAppHTML = changeAppHTML;
+})(this);
+
+changeAppHTML("Favourite Drink: seltzer");
